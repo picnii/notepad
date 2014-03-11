3dfc9a54f5405ee86a76fa6c91701a802f9128d8
picnii <sompop@picnii.me>
2014-03-11 10:17:41 +0700

    update editor ctrl

--- js/Controller.js
+++ js/Controller.js
@@ -4,4 +4,9 @@ function MainCtrl ($scope) {
 
 function EditorCtrl ($scope) {
 	// body...
+	$scope.save = function()
+	{
+		var save_object = {name:$scope.subject, content:$scope.content};
+		console.log(save_object);
+	}	
 }
