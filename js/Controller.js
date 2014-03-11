3a57b1dc1e20348c1d32fd9dff2c22e0077eff95
picnii <sompop@picnii.me>
2014-03-11 10:40:18 +0700

    update editor ctrl to save

--- js/Controller.js
+++ js/Controller.js
@@ -8,5 +8,18 @@ function EditorCtrl ($scope) {
 	{
 		var save_object = {name:$scope.subject, content:$scope.content};
 		console.log(save_object);
+
+		//check if there are localstorage
+		if(typeof(localStorage['notes']) == 'undefined')
+		{
+			var save_array = [];
+			save_array.push(save_object);
+			localStorage['notes'] = JSON.stringify(save_array);
+		}else
+		{
+			save_array = JSON.parse(localStorage['notes']);
+			save_array.push(save_object);
+			localStorage['notes'] = JSON.stringify(save_array);
+		}
 	}	
 }
